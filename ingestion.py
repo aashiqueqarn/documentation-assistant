162568fa24103aecbb7482bfbf834fef3c801c32
Aashique Karn <aashiquekarn@gmail.com>
2026-01-14 10:46:10 +0530

    master | initial commit

---
+++ ingestion.py
@@ -0,0 +1,20 @@
+import asyncio
+import os
+import ssl
+from typing import Any, Dict, List
+
+import certifi
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+
+async def main():
+    pass
+
+if __name__ == "__main__":
+    asyncio.run(main())
